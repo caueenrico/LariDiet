7a3d84463b5c839e757231cb0b428fb4c2150258
caueenrico <caueenrico@gmail.com>
2024-03-19 03:22:26 -0300

    boraTestarEmProdução kk

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.register(mealsRoutes, {
 })
 
 app.listen({
-  port: env.PORT
+  host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
 }).then(() => {
   console.log('o servidor está rodando na porta ' + env.PORT)
 })
