f3b32b00a0fd76b698ae2e2c4fd39a771cc6a8f0
caueenrico <caueenrico@gmail.com>
2024-03-19 03:39:14 -0300

    boraTestarmigration

--- db/migrations/20240315230543_create-meals.ts
+++ db/migrations/20240315230543_create-meals.ts
@@ -3,8 +3,8 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("meals", (table) => {
-    table.uuid("id").primary();
-    table.integer("user_id").references("users.id").notNullable()
+    table.increments("id").primary();
+    table.uuid("user_id").references("users.id").notNullable()
     table.string("mealsName").notNullable()
     table.string("description").nullable()
     table.date("date").notNullable()
